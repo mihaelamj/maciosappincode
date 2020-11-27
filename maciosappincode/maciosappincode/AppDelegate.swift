a894c52b15e83d0965a737dda787e7ace455a052
Mihaela MJ <mihaelamj@me.com>
2020-11-27 18:14:46 +0100

    wip

---
+++ maciosappincode/maciosappincode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  maciosappincode
+//
+//  Created by Mihaela Mihaljevic Jakic on 27.11.2020..
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
